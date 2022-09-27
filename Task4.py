150c63fb0a7fc33e7e55a52340a213b5a6eae516
Nadezhda <S_Nadja@bk.ru>
2022-09-27 19:52:48 +0300

    Task 4 is done

---
+++ Task4.py
@@ -0,0 +1,15 @@
+##### ** Task 4 ** #####
+# Напишите программу, которая по заданному номеру четверти, 
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quarter = int(input('Input number of quarter: '))
+if quarter == 1:
+    print('x ∈ (0; + ∞), y ∈ (0; + ∞)')
+elif quarter == 2:
+    print('x ∈ (- ∞; 0), y ∈ (0; + ∞)')
+elif quarter == 3:
+    print('x ∈ (- ∞; 0), y ∈ (- ∞; 0)')
+elif quarter == 4:
+    print('x ∈ (0; + ∞), y ∈ (- ∞; 0)')
+else:
+    print('Number of quarter is not correct!')
